b51ce5cd3ed0da14e36f1d94456d962837b22a0d
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-25 12:06:21 +0400

    Update subscription.rb

--- app/models/locomotive_subscription/subscription.rb
+++ app/models/locomotive_subscription/subscription.rb
@@ -8,7 +8,7 @@ module LocomotiveSubscription
       @emails = Subscription.all.map {|subs| subs.email}
       @news = Locomotive::ContentType.where(slug: 'news').first.entries.where(:updated_at.gte => Date.today-100.day)
       if @news.count > 0
-        @email.each do |email|
+        @emails.each do |email|
           SenderMailer.send_news(email,@news).deliver
         end
       end
