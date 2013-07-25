0793387a7ebcd4a9d9cb018e220acdadb044415f
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-25 11:34:13 +0400

    Update sender_mailer.rb

--- app/mailers/locomotive_subscription/sender_mailer.rb
+++ app/mailers/locomotive_subscription/sender_mailer.rb
@@ -4,16 +4,11 @@ module LocomotiveSubscription
     default from: "info@kdm69.ru",
             subject: "Все новости КДМ ТО"
 
-    def send_news
+    def send_news(email,news)
         @address = "http://198.199.124.29:7777"
-        @emails = Subscription.all.map{ |subs| subs.email }
-        @news = Locomotive::ContentType.where(slug: 'news').first.entries.where(:updated_at.gte => Date.today-100.day)
-        if @news.count > 0
-            @emails.each do |email|
-                mail(to: email).deliver!
-            end
-        end
-        
+        @news = news
+        mail(to: email)
     end
+    
   end
 end
