5b38d0ed2d03bafaef222094568f6925ee9887e4
Alex Nigomatulin <alex4578@yandex.ru>
2013-07-25 11:33:13 +0400

    Update subscription.rb

--- app/models/locomotive_subscription/subscription.rb
+++ app/models/locomotive_subscription/subscription.rb
@@ -2,5 +2,15 @@ module LocomotiveSubscription
   class Subscription
     include Locomotive::Mongoid::Document
     field :email, type: String
+    
+    def self.send_news
+      @emails = Subscription.all.map {|subs| subs.email}
+      @news = Locomotive::ContentType.where(slug: 'news').first.entries.where(:updated_at.gte => Date.today-100.day)
+      if @news.count > 0
+        @email.each do |email|
+          SenderMailer.send_news(email,@news).deliver
+        end
+      end
+    end
   end
 end
